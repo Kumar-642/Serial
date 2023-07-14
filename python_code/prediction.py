564a066ead0c0cc82bf0c005245ae5c299c4661e
Kumar-642 <128512495+Kumar-642@users.noreply.github.com>
2023-07-14 13:26:38 +0530

    prediction.py

--- python_code/prediction.py
+++ python_code/prediction.py
@@ -1,4 +1,4 @@
-from joblib import load
+from joblib import load    # We already dump our model on joblib file, whenever we require we call it .
 
 def prediction():
   print('model can predict in range of 0 - 1000 ml')
@@ -8,7 +8,7 @@ def prediction():
       return print('Enter the value between 0-1000 ml or gm')
   except:
     return print('You entered invalid input')
-  test_model = load('/Users/anchalkumarchaubey/Desktop/LDI Pro system/python_code/linear_model.joblib')
+  test_model = load('/Users/anchalkumarchaubey/Desktop/LDI Pro system/python_code/linear_model.joblib') # we call regression model from joblib
   sequence = test_model.predict([[weight]])
   hrs = (sequence*5)/60
   return print(hrs[0,0],'hours')
