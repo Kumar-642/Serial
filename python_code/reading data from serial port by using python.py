357cc6371e15cbf62874d798c683c7e47f967a56
Kumar-642 <128512495+Kumar-642@users.noreply.github.com>
2023-07-14 12:36:55 +0530

    Update reading data from serial port by using python.py

--- python_code/reading data from serial port by using python.py
+++ python_code/reading data from serial port by using python.py
@@ -1,16 +1,19 @@
-import serial
+import serial     # Importing Serial Library (this library does not work on Google collab)
+
+# importing necessary library
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import time
-print('Remove any weight before starting')
-time.sleep(5)
-ser = serial.Serial(port='/dev/cu.usbmodem11301',baudrate=57600)
+
+print('Remove any weight before starting')    # Please empty your Load cell 
+time.sleep(5)                                  # Wait to 5 seconds for empty 
+ser = serial.Serial(port='/dev/cu.usbmodem11301',baudrate=57600)   # port name might be different for your system
 print('Intialising setup please wait....')
 time.sleep(1)
-print('Put the weight on machine')
-timer = 11
+print('Put the weight on machine')    # Put the weight on Load cell 
+timer = 11                            # Time gap of 11 seconds so that you can put weight on Load cell
 for i in range(11):
     time.sleep(1)
     timer-=1
