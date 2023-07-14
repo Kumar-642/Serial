806f01e2e351a676070654d8332eb53b0c16e80d
Kumar-642 <128512495+Kumar-642@users.noreply.github.com>
2023-07-14 13:43:30 +0530

    prediction.py

--- python_code/prediction.py
+++ python_code/prediction.py
@@ -1,21 +1,19 @@
-from joblib import load    # We already dump our linear regression model on joblib file, whenever we require we call it .
+# Here we make linear regression model to predict the time required to flow out x gm amount of water.
+# please keep 'linear_model.joblib' and 'prediction.py' file in same folder.
+
+from joblib import load    # We already dump our linear regression model on joblib file, whenever we require, we call it .
 
 def prediction():
-  print('model can predict in range of 0 - 1000 ml')
+  print('model can predict in range of 0 - 1000 gm')
   try:
-    weight = int(input('Enter The weight in gm/ml to predict:'))
+    weight = int(input('Enter The weight in gm/ml to predict:')) # the time required to flow out x gm amount of water.
     if not (weight > 0 and weight < 1000):
       return print('Enter the value between 0-1000 ml or gm')
   except:
-    return print('You entered invalid input')
+    return print('You entered invalid input') 
   test_model = load('/Users/anchalkumarchaubey/Desktop/LDI Pro system/python_code/linear_model.joblib') # we call regression model from joblib
   sequence = test_model.predict([[weight]])
   hrs = (sequence*5)/60
   return print(hrs[0,0],'hours')
 
-prediction()
-prediction()
-prediction()
-prediction()
-prediction()
 prediction()
